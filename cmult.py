36530a6da626c8194e6d9b60308c2a274be71cc7
Omar <omaraflak@google.com>
2024-05-16 10:22:37 +0000

    rm useless .h

--- cmult.py
+++ cmult.py
@@ -5,5 +5,6 @@ libname = pathlib.Path().absolute() / "libcmult.so"
 c_lib = ctypes.CDLL(libname)
 c_lib.cmult.restype = ctypes.c_float
 
+
 def cmult(a: int, b: float) -> float:
-    return c_lib.cmult(a, ctypes.c_float(b))
+    return c_lib.cmult(ctypes.c_int(a), ctypes.c_float(b))
